dc7d428a87e7c2069ae2b0b54ea418f7e77f5719
Aryaneel-shivam <49811270+Aryaneel-shivam@users.noreply.github.com>
2019-08-05 22:03:46 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+var sound = new Audio();
+sound.src = "sound1.mp3"
+var sounda = new Audio();
+sounda.src = "sound2.mp3"
+var soundb = new Audio();
+soundb.src = "sound3.mp3"
+var soundc = new Audio();
+soundc.src = "sound4.mp3"
+var soundd = new Audio();
+soundd.src = "sound5.wav"
+var sounde = new Audio();
+sounde.src = "sound6.wav"
+var soundf = new Audio();
+soundf.src = "sound9.wav"
+var soundg = new Audio();
+soundg.src  = "sound7.wav"
+var soundh = new Audio();
+soundh.src = "sound8.wav"
+var soundi = new Audio();
+soundi.src = "sound10.wav"
+var soundj = new Audio();
+soundj.src = "sound11.wav"
+var soundk = new Audio;
+soundk.src = "sound12.wav"
+
+
+
+
+
